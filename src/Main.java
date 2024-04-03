3cb6bc2894543dc25f790cdad082a37d574de518
Zane41 <korener@rambler.ru>
2024-04-03 14:37:16 +0300

    исправление ошибок №1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
         System.out.println("Введи длину массива");
         Scanner vvod = new Scanner(System.in);
         int l = vvod.nextInt();
@@ -14,38 +11,26 @@ public class Main {
             mass1[i] = vvod.nextInt();
         }
         System.out.println("Самая длинная оттепель длилась " + printFromX(mass1));
-
     }
-
-
     public static int printFromX(int[] m) {
-
         int returningValue1 = 0;
         int countPlusTemp=0;
         for (int i = 0; i < m.length; i++) {
             if (sravn(m[i], 0))
                 countPlusTemp++;
             else {
-                if (countPlusTemp > returningValue1){
+                if (countPlusTemp >= returningValue1){
                     returningValue1 = countPlusTemp; countPlusTemp=0;
                 }
             }
             if (i==m.length-1)
-                if (sravn(m[i], 0)) {
-                    returningValue1 = countPlusTemp;
-                }
-                else {
-                    if (countPlusTemp > returningValue1){
-                        returningValue1 = countPlusTemp; countPlusTemp=0;
-                    }
+                if (countPlusTemp > returningValue1){
+                   returningValue1 = countPlusTemp;
                 }
         }
         return returningValue1;
     }
-
     public static boolean sravn(int x, int y) {
         return x > y;
     }
-
-
 }
